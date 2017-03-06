0eeb21bae4b1bb073ad764f48b57faedf27a0a82
OctoClo <octoclo@zoho.com>
2017-03-06 23:38:00 +0100

    Spawn a tetromino on each tetromino landing, check collisions between tetrominos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,22 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public GameObject[] tetrominos;
+    // Tetrominos that can be spawned later on
+    public Tetromino[] tetrominos;
 
-    private int boardWidth = 10;
-    private int boardHeigt = 19;
+    // Dimensions of the board
+    private static int boardWidth = 10;
+    private static int boardHeight = 19;
+
+    // Array representing each cell of the board and its mino if any
+    private static Transform[,] boardMinos = new Transform[boardWidth, boardHeight];
 
 	void Start ()
     {
+        // Spawn the first tetromino
         SpawnNextTetromino();
 	}
 	
-	void Update ()
+	public void UpdateBoard (Tetromino tetromino)
     {
-		
-	}
+        // For each cell in the game board
+        for (int x = 0; x < boardWidth; x++)
+        {
+            for (int y = 0; y < boardHeight; y++)
+            {
+                // If the cell's mino's parent (i.e. tetromino) equals the one we were given
+                if (boardMinos[x, y] != null)
+                    if (boardMinos[x, y].parent == tetromino.transform)
+                        // Delete it, because we're moving that tetromino
+                        boardMinos[x, y] = null;
+            }
+        }
 
-    private void SpawnNextTetromino()
+        // Then, update the current position
+        foreach (Transform mino in tetromino.transform)
+        {
+            Vector2 position = Round(mino.position);
+
+            if (position.y > 0)
+                boardMinos[(int)position.x, (int)position.y] = mino;
+        }
+    }
+
+    public Transform getMinoAtPosition (Vector2 position)
+    {
+        if (position.y <= 0)
+            return null;
+        else
+            return boardMinos[(int)position.x, (int)position.y];
+    }
+
+    public void SpawnNextTetromino()
     {
         // Random index
         int index = Random.Range(0, tetrominos.Length);
@@ -30,7 +64,7 @@ public class GameManager : MonoBehaviour {
 
     public bool IsInsideBoard (Vector2 position)
     {
-        return ((int)position.x >= 0 && (int)position.x < boardWidth && (int)position.y < boardHeigt);
+        return ((int)position.x >= 0 && (int)position.x < boardWidth && (int)position.y < boardHeight);
     }
 
     public Vector2 Round (Vector2 position)
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -4,37 +4,38 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour {
 
-    // Not allowed to rotate : Tetromino O
+    // Tetromino not allowed to rotate : O
     public bool allowedToRotate = true;
 
-    // Rotate only twice : Tetrominos I, S and Z
+    // Tetrominos that rotate only twice : I, S and Z
     public bool rotateTwiceOnly = false;
-    
-    // GameManager instance to call manage move checking
+
+    // GameManager instance to manage move checking
     private GameManager manager;
     
-    // List of 3 Vector3 to move the tetromino depending on its direction (LEFT, RIGHT and UP)
+    // List of 3 Vector3 to move the tetromino depending on its direction (LEFT, RIGHT, UP and DOWN)
     private Vector3[] move = { new Vector3(-1, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, -1, 0) };
     private const int LEFT = 0;
     private const int RIGHT = 1;
     private const int UP = 2;
     private const int DOWN = 3;
 
-    // Last time the tetromino falled
+    // Last time the tetromino fell
     private float previousFallTime;
 
     // Speed the tetromino is falling at (default : 1 unit per second)
     private float speed = 1f;
 
-    private void Awake ()
+    void Awake ()
     {
+        // Get the instance of GameManager
         manager = FindObjectOfType<GameManager>();
     }
 
-    void Start ()
+    void Start()
     {
-		
-	}
+        previousFallTime = Time.time;
+    }
 
     void Update ()
     {
@@ -43,74 +44,94 @@ public class Tetromino : MonoBehaviour {
 
     void ProcessInput ()
     {
-        // Move left
+        // Arrow left = Move left
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += move[LEFT];
 
-            if (!validPosition())
+            // If the tetromino is allowed to stay, update the board according to its new position
+            if (validPosition())
+                manager.UpdateBoard(this);
+            // Else, abort, abort !
+            else
                 transform.position += move[RIGHT];
         }
             
 
-        // Move right
+        // Arrow right = Move right
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += move[RIGHT];
 
-            if (!validPosition())
+            // If the tetromino is allowed to stay, update the board according to its new position
+            if (validPosition())
+                manager.UpdateBoard(this);
+            // Else, abort, abort !
+            else
                 transform.position += move[LEFT];
         }
 
-        // Rotate by 90 degres
+        // Arrow down = Rotate by 90 degres
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             if (allowedToRotate)
             {
+                // If the tetromino rotates only twice, change its rotation based on its current rotation
                 if (rotateTwiceOnly)
-                {
-                    if (transform.rotation.eulerAngles.z >= 90)
-                        transform.Rotate(0, 0, -90);
-                    else
-                        transform.Rotate(0, 0, 90);
-                }
+                    transform.Rotate(0, 0, (transform.rotation.eulerAngles.z >= 90) ? -90 : 90);
                 else
                     transform.Rotate(0, 0, 90);
 
-                if (!validPosition())
+                // If the tetromino is allowed to stay, update the board according to its new position
+                if (validPosition())
+                    manager.UpdateBoard(this);
+                // Else, abort, abort !
+                else
                 {
                     if (rotateTwiceOnly)
-                    {
-                        if (transform.rotation.eulerAngles.z >= 90)
-                            transform.Rotate(0, 0, -90);
-                        else
-                            transform.Rotate(0, 0, 90);
-                    }
+                        transform.Rotate(0, 0, (transform.rotation.eulerAngles.z >= 90) ? -90 : 90);
                     else
                         transform.Rotate(0, 0, -90);
                 }   
             }
         }
 
-        // Move up or fall if it's been 1 second since last fall
-        else if (Input.GetKeyDown(KeyCode.UpArrow) || Time.time - previousFallTime >= speed)
+        // Arrow up | n seconds (1 unit / speed) since last fall = Move up
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Time.time - previousFallTime >= (1 / speed))
         {
             transform.position += move[UP];
 
-            if (!validPosition())
-                transform.position += move[DOWN];
+            // If the tetromino is allowed to stay, update the board according to its new position
+            if (validPosition())
+            {
+                manager.UpdateBoard(this);
+            }
+            // Else, abort, abort ! It means the tetromino has reached the top border, disable it and spawn a new tetromino
             else
-                previousFallTime = Time.time;
+            {
+                transform.position += move[DOWN];
+                enabled = false;
+                manager.SpawnNextTetromino();
+            }
+
+            previousFallTime = Time.time;
         }
     }
 
     private bool validPosition ()
     {
+        // For each mino in the tetromino
         foreach (Transform mino in transform)
         {
             Vector2 position = manager.Round(mino.position);
+
+            // Check if it is inside the board
             if (!manager.IsInsideBoard(position))
                 return false;
+            // And if its parent is us : else it means it's hitting some other tetromino
+            else if (manager.getMinoAtPosition(position) != null)
+                if (manager.getMinoAtPosition(position).parent != transform)
+                    return false;
         }
 
         return true;
