789f3b65163f4862d04fd6af99a040c7a0e35f04
OctoClo <octoclo@zoho.com>
2017-03-06 21:50:47 +0100

    Correct tetrominos initial position, check moves

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private int boardWidth = 10;
+    private int boardHeigt = 19;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public bool IsInsideBoard (Vector2 position)
+    {
+        Debug.Log("X : " + (int)position.x + " - Y : " + (int)position.y);
+        return ((int)position.x >= 0 && (int)position.x < boardWidth && (int)position.y < boardHeigt);
+    }
+
+    public Vector2 Round (Vector2 position)
+    {
+        return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
+    }
+}
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -3,34 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour {
+
+    private GameManager manager;
+    
+    // List of 3 Vector3 to move the tetromino depending on its direction (LEFT, RIGHT and UP)
+    private Vector3[] move = { new Vector3(-1, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, -1, 0) };
+    private const int LEFT = 0;
+    private const int RIGHT = 1;
+    private const int UP = 2;
+    private const int DOWN = 3;
+
+    // Last time the tetromino falled
     private float previousFallTime;
+
+    // Speed the tetromino is falling at (default : 1 unit per second)
     private float speed = 1f;
 
-	void Start ()
+    private void Awake ()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    void Start ()
     {
 		
 	}
 
     void Update ()
     {
-        processInput();
+        ProcessInput();
 	}
 
-    void processInput ()
+    void ProcessInput ()
     {
+        // Move left
         if (Input.GetKeyDown(KeyCode.LeftArrow))
-            transform.position += new Vector3(-1, 0, 0);
+        {
+            transform.position += move[LEFT];
 
+            if (!CheckPosition())
+                transform.position += move[RIGHT];
+        }
+            
+
+        // Move right
         else if (Input.GetKeyDown(KeyCode.RightArrow))
-            transform.position += new Vector3(1, 0, 0);
+        {
+            transform.position += move[RIGHT];
 
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (!CheckPosition())
+                transform.position += move[LEFT];
+        }
+
+        // Rotate by 90 degres
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             transform.Rotate(0, 0, 90);
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - previousFallTime >= speed)
-        {
-            transform.position += new Vector3(0, -1, 0);
-            previousFallTime = Time.time;
+            if (!CheckPosition())
+                transform.Rotate(0, 0, -90);
         }
+
+        // Move up or fall if it's been 1 second since last fall
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Time.time - previousFallTime >= speed)
+        {
+            transform.position += move[UP];
+
+            if (!CheckPosition())
+                transform.position += move[DOWN];
+            else
+                previousFallTime = Time.time;
+        }
+    }
+
+    private bool CheckPosition ()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 position = manager.Round(mino.position);
+            if (!manager.IsInsideBoard(position))
+                return false;
+        }
+
+        return true;
     }
 }
