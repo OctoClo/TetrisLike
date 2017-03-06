7b79bc28a50d68d88e1395799742e2fba0d2b60c
OctoClo <octoclo@zoho.com>
2017-03-06 20:27:23 +0100

    Tetrominos prefabs & script to move and fall + Game Board prefab

---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour {
+    private float previousFallTime;
+    private float speed = 1f;
+
+	void Start ()
+    {
+		
+	}
+
+    void Update ()
+    {
+        processInput();
+	}
+
+    void processInput ()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.position += new Vector3(-1, 0, 0);
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.position += new Vector3(1, 0, 0);
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.Rotate(0, 0, 90);
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - previousFallTime >= speed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            previousFallTime = Time.time;
+        }
+    }
+}
